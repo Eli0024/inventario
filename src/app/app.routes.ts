9a348966b97800039c73339180487eaae4c99cf6
Eli0024 <eliana199972@gmail.com>
2024-10-31 22:47:35 -0500

    YES

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FormcompComponent } from './formcomp/formcomp.component';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: '/content', pathMatch: 'full' },
@@ -9,4 +9,5 @@ export const routes: Routes = [
     { path: 'form1', loadChildren: () => import('./formcomp/formcomp.module').then(m => m.FormcompModule) },
     { path: 'form2', loadChildren: () => import('./formuser/formuser.module').then(m => m.FormuserModule) },
     { path: 'form3', loadChildren: () => import('./formoff/formoff.module').then(m => m.FormoffModule) },
+    { path: 'info', loadChildren: () => import('./info/info.module').then(m => m.InfoModule) },
 ];
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink } from '@angular/router';
+import { InfoComponent } from '../info/info.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [NavbarComponent, RouterLink],
+  imports: [NavbarComponent, RouterLink, InfoComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
