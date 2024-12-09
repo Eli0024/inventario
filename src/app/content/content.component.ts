ee0b21c542564d536de12f70af13a45b68896bcc
Eli0024 <eliana199972@gmail.com>
2024-12-09 15:49:54 -0500

    DESIGN

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink } from '@angular/router';
 import { InfoComponent } from '../info/info.component';
@@ -6,6 +6,8 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FormcompComponent } from '../formcomp/formcomp.component';
+import { Usuario } from '../models/users';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-content',
@@ -14,12 +16,53 @@ import { FormcompComponent } from '../formcomp/formcomp.component';
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit{
  
   showSidebar = true; // Controla la visibilidad del sidebar
 
   toggleSidebar() {
     this.showSidebar = !this.showSidebar;
   }
-}
+  
+  constructor(private usersService: UsersService, private cdr: ChangeDetectorRef) {  }
 
+  ngOnInit(): void {
+    this.loadUsuarios();
+    }
+
+    loadUsuarios(): void {
+      this.usersService.getAll().subscribe(
+        (response: Usuario[]) => {
+          this.usuarios = response;
+        },
+        (error: any) => {
+          console.error('Error al obtener los usuarios', error);
+        }
+      );
+    }
+
+  usuarios: Usuario[] = [];
+
+  usuario: Usuario = {
+    id_usuario: 0,
+    nombre: '',
+    apellido: '',
+    empresa: '',
+    area: '',
+    cargo: '',
+    licencia: null,
+  };
+
+  filter: any = { searchTerm: '' };
+  
+  filterUsuarios(): Usuario[] {
+    if (this.usuarios && this.usuarios.length) {
+      return this.usuarios.filter(Usuario =>
+        Usuario.nombre.toLowerCase().includes(this.filter.searchTerm.toLowerCase()) ||
+        Usuario.apellido.toLowerCase().includes(this.filter.searchTerm.toLowerCase()) ||
+        Usuario.empresa.toLowerCase().includes(this.filter.searchTerm.toLowerCase())
+      );
+    }
+    return [];
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       colors: {
         'dark-blue': '#1F4172',
+        'custom-blue': 'rgb(42, 124, 190)',
     },
     backgroundImage: {
       'hero': "url('https://4kwallpapers.com/images/walls/thumbs_3t/10876.jpg')"
